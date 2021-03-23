a94ecad93068b0c0f309089fe58d98cc06461b5d
Robi Enjalbert <robin.enjalbert@inria.fr>
2021-03-23 16:55:21 +0100

    Test and fix Environment.

--- DeepPhysX/Environment/Environment.py
+++ DeepPhysX/Environment/Environment.py
@@ -3,15 +3,22 @@ import numpy as np
 
 class Environment:
 
-    def __init__(self, simulations_per_step=1):
+    def __init__(self, simulations_per_step=1, max_wrong_samples_per_step=10):
+
         self.simulationPerStep = simulations_per_step
+        self.maxWrongSamplesPerStep = max_wrong_samples_per_step
+
         self.inputs = np.array([])
         self.outputs = np.array([])
         self.inputSize = None
         self.outputSize = None
 
+        self.description = ""
+
+        self.create()
+
     def create(self):
-        raise NotImplementedError
+        print("WARNING: You have to implement environment create() method.")
 
     def reset(self):
         raise NotImplementedError
@@ -24,3 +31,15 @@ class Environment:
 
     def getOutput(self):
         return self.outputs
+
+    def checkSample(self):
+        raise NotImplementedError
+
+    def getDescription(self):
+        if len(self.description) == 0:
+            self.description += "\nCORE Environment:\n"
+            self.description += "   Simulations per step: {}\n".format(self.simulationPerStep)
+            self.description += "   Max wrong samples per step: {}\n".format(self.maxWrongSamplesPerStep)
+            self.description += "   Inputs, size: {}\n".format(self.inputs, self.inputSize)
+            self.description += "   Outputs, size: {}\n".format(self.outputs, self.outputSize)
+        return self.description
--- DeepPhysX/Environment/EnvironmentConfig.py
+++ DeepPhysX/Environment/EnvironmentConfig.py
@@ -1,13 +1,29 @@
-from .Environment import Environment
+import os
 
 
 class EnvironmentConfig:
 
-    def __init__(self, simulation_per_step=1, always_create_data=False):
-
-        self.environment = Environment
-        self.environmentConfig = simulation_per_step
+    def __init__(self, environment_class, simulation_per_step=1, max_wrong_samples_per_step=10,
+                 always_create_data=False, multiprocessing=1):
+        # Environment variables
+        self.environment_class = environment_class
+        self.environmentConfig = simulation_per_step, max_wrong_samples_per_step
+        # EnvironmentManager variables
         self.alwaysCreateData = always_create_data
+        self.multiprocessing = min(max(multiprocessing, 1), os.cpu_count())  # Assert nb is between 1 and cpu_count
+        # Description
+        self.description = ""
 
     def createEnvironment(self):
-        return self.environment(self.environmentConfig)
+        if self.multiprocessing == 1:
+            return self.environment_class(*self.environmentConfig)
+        return [self.environment_class(*self.environmentConfig) for _ in range(self.multiprocessing)]
+
+    def getDescription(self):
+        if len(self.description) == 0:
+            self.description += "\nCORE EnvironmentConfig:\n"
+            self.description += "   (environment) Environment class: {}\n".format(self.environment_class.__name__)
+            self.description += "   (environment) Environment config: {}\n".format(self.environmentConfig)
+            self.description += "   (environmentManager) Always create data: {}\n".format(self.alwaysCreateData)
+            self.description += "   (environmentManager) Multiprocessing value: {}\n".format(self.multiprocessing)
+        return self.description
