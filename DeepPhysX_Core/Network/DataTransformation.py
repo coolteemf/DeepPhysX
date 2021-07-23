8ec9b5af6aa9106d6ef693e26629063c81ecd77e
Robin <robin.enjalbert@inria.fr>
2021-07-23 09:25:35 +0200

    Update doc of DataTransformation.

--- DeepPhysX_Core/Network/DataTransformation.py
+++ DeepPhysX_Core/Network/DataTransformation.py
@@ -1,6 +1,13 @@
 class DataTransformation:
 
     def __init__(self, network_config):
+        """
+        DataTransformation is dedicated to data operations before and after network predictions.
+
+        :param network_config: BaseNetworkConfig class
+        """
+        self.name = self.__class__.__name__
+
         self.network_config = network_config
         self.data_type = any
 
@@ -9,16 +16,34 @@ class DataTransformation:
         def inner(self, *args):
             for data in args:
                 if type(data) != self.data_type:
-                    raise TypeError(f"[DataTransformation] The given data must be of type {self.data_type}, "
-                                    f"found {type(data)}")
+                    raise TypeError(f"[{self.name}] Wrong data type: {self.data_type} required, get {type(data)}")
             return func(self, *args)
         return inner
 
     def transformBeforePrediction(self, data_in):
+        """
+        Apply data operations before network's prediction.
+
+        :param data_in: Input data
+        :return: Transformed input data
+        """
         return data_in
 
     def transformBeforeLoss(self, data_out, data_gt):
+        """
+        Apply data operations between network's prediction and loss computation.
+
+        :param data_out: Prediction data
+        :param data_gt: Ground truth data
+        :return: Transformed prediction data, transformed ground truth data
+        """
         return data_out, data_gt
 
     def transformBeforeApply(self, data_out):
+        """
+        Apply data operations between loss computation and prediction apply in environment.
+
+        :param data_out: Prediction data
+        :return: Transformed prediction data
+        """
         return data_out
