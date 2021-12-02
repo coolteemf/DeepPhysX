a162d999fd16c18b310c1e87fbb52e370d9f2c9d
aodot <alban.odot@inria.fr>
2021-12-02 14:26:00 +0100

    Similar init to core

--- DeepPhysX_Sofa/__init__.py
+++ DeepPhysX_Sofa/__init__.py
@@ -8,7 +8,9 @@ for rootdir, dirs, files in walk(current_absolute_path):
         for file in files:
             if "pyc" not in file and file != "__init__.py":
                 splitted_path = str(rootdir).split(sep)
-                main_lib = splitted_path[-2]        # DeepPhysX_Core
+                main_lib = splitted_path[-2]        # DeepPhysX_Sofa
+                if "Example" in main_lib:
+                    continue
                 sub_lib = splitted_path[-1]         # Pipeline, utils, Network...
                 filename = str(file).split(".")[0]  # BaseNetwork, TcpIpClient, DatasetManager...
                 exec(f"from {main_lib}.{sub_lib}.{filename} import *")
