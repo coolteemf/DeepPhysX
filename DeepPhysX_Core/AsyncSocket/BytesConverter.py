3a5b38cfd4ad70293dd9dc120067b7afa78ac33a
aodot <alban.odot@inria.fr>
2021-12-06 20:07:44 +0100

    removed numpy array cast of bool, float etc

--- DeepPhysX_Core/AsyncSocket/BytesConverter.py
+++ DeepPhysX_Core/AsyncSocket/BytesConverter.py
@@ -1,6 +1,6 @@
 from numpy import ndarray, array, frombuffer, zeros
 
-
+from struct import pack, unpack, calcsize
 class BytesConverter:
 
     def __init__(self):
@@ -9,31 +9,22 @@ class BytesConverter:
         Available types: bytes, str, bool, signed int, float, list, array
         """
 
-        self.int_size = 4
-
-        # Data to bytes conversions
+        self.int_size = calcsize("i")
+        #Data to bytes conversions
         self.__data_to_bytes_conversion = {bytes: lambda d: d,
-<<<<<<< HEAD
-<<<<<<< HEAD
                                            str: lambda d: d.encode('utf-8'),
-=======
-                                           str: lambda d: d.lower().encode('utf-8'),
->>>>>>> Updated visualizer achitecture
-=======
-                                           str: lambda d: d.encode('utf-8'),
->>>>>>> Server and client handle multiple object types and dictionaries
-                                           bool: lambda d: int(d).to_bytes(1, byteorder='big'),
-                                           int: lambda d: d.to_bytes(length=self.int_size, byteorder='big', signed=True),
-                                           float: lambda d: array(d, dtype=float).tobytes(),
+                                           bool: lambda d: bytearray(pack('?', d)),
+                                           int: lambda d: bytearray(pack('i', d)),
+                                           float: lambda d: bytearray(pack('f', d)),
                                            list: lambda d: array(d, dtype=float).tobytes(),
                                            ndarray: lambda d: array(d, dtype=float).tobytes()}
 
         # Bytes to data conversions
         self.__bytes_to_data_conversion = {bytes.__name__: lambda b: b,
                                            str.__name__: lambda b: b.decode('utf-8'),
-                                           bool.__name__: lambda b: bool(int.from_bytes(b, byteorder='big')),
-                                           int.__name__: lambda b: int.from_bytes(b, byteorder='big', signed=True),
-                                           float.__name__: lambda b: frombuffer(b).item(),
+                                           bool.__name__: lambda b: unpack('?', b)[0],
+                                           int.__name__: lambda b: unpack('i', b)[0],
+                                           float.__name__: lambda b: unpack('f', b)[0],
                                            list.__name__: lambda b, t, s: frombuffer(b).astype(t).reshape(s).tolist(),
                                            ndarray.__name__: lambda b, t, s: frombuffer(b).astype(t).reshape(s)}
 
@@ -57,7 +48,6 @@ class BytesConverter:
         # Convert the type of 'data' from str to bytes
         type_data = self.__data_to_bytes_conversion[str](type(data).__name__)
         sizes += (self.size_to_bytes(type_data),)
-
         # Convert 'data' to bytes
         data_bytes = self.__data_to_bytes_conversion[type(data)](data)
         sizes += (self.size_to_bytes(data_bytes),)
@@ -66,6 +56,7 @@ class BytesConverter:
         args = ()
         # Shape and data type for list and array
         if type(data) in [list, ndarray]:
+
             # Get python native datatype of array
             dtype = type(zeros(1, dtype=array(data).dtype).item()).__name__
             # Convert datatype of array from str to bytes
@@ -92,80 +83,6 @@ class BytesConverter:
         return bytes_message
 
     def bytes_to_data(self, bytes_fields):
-        """
-        Recover data from bytes fields.
-        Available types: bytes, str, bool, signed int, float, list, array.
-
-        :param bytes_fields: (Type, Data, *args)
-        :return: Recovered data
-        """
-
-        # Recover the data type
-        data_type = self.__bytes_to_data_conversion[str.__name__](bytes_fields[0])
-
-        # Recover additional arguments
-        args = ()
-        # Shape and data type for list and array
-        if data_type in [list.__name__, ndarray.__name__]:
-            # Recover datatype of array
-            args += (self.__bytes_to_data_conversion[str.__name__](bytes_fields[2]),)
-            # Recover shape of array
-            args += (self.__bytes_to_data_conversion[ndarray.__name__](bytes_fields[3], int, -1),)
-
-        # Convert bytes to data
-        return self.__bytes_to_data_conversion[data_type](bytes_fields[1], *args)
-
-    def data_to_bytes2(self, data, as_list=False):
-        """
-        Convert data to bytes.
-        Available types: bytes, str, bool, signed int, float, list, array.
-
-        :param data: Data to convert.
-        :param as_list: (for tests) If True, the return will be a list of bytes fields. If False, the whole bytes
-        message is returned
-        :return: bytes_fields: Size of tuple in bytes, (Type, Data, *args)
-        """
-
-        # Store the sizes of the bytes fields, sizes will have a constant number of 4 bytes
-        sizes = ()
-        # Convert the type of 'data' from str to bytes
-        type_data = self.__data_to_bytes_conversion[str](type(data).__name__)
-        sizes += (self.size_to_bytes(type_data),)
-
-        # Convert 'data' to bytes
-        data_bytes = self.__data_to_bytes_conversion[type(data)](data)
-        sizes += (self.size_to_bytes(data_bytes),)
-
-        # Additional arguments
-        args = ()
-        # Shape and data type for list and array
-        if type(data) in [list, ndarray]:
-            # Get python native datatype of array
-            dtype = type(zeros(1, dtype=array(data).dtype).item()).__name__
-            # Convert datatype of array from str to bytes
-            dtype_bytes = self.__data_to_bytes_conversion[str](dtype)
-            args += (dtype_bytes,)
-            sizes += (self.size_to_bytes(dtype_bytes),)
-            # Convert data shape from array to bytes
-            shape_bytes = self.__data_to_bytes_conversion[ndarray](array(data).shape)
-            args += (shape_bytes,)
-            sizes += (self.size_to_bytes(shape_bytes),)
-
-        # Convert the number of bytes fields to bytes
-        nb_fields = self.__data_to_bytes_conversion[int](2 + len(args))
-
-        # Gather all bytes fields in the desired order
-        fields = [nb_fields, *sizes, type_data, data_bytes, *args]
-        if as_list:
-            return fields
-
-        # Concatenate bytes fields
-        bytes_message = fields[0]
-        for f in fields[1:]:
-            bytes_message += f
-        return bytes_message
-
-    def bytes_to_data2(self, bytes_fields):
         """
         Recover data from bytes fields.
         Available types: bytes, str, bool, signed int, float, list, array.
