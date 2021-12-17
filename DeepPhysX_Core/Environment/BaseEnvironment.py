10f8a56b6fe0b7f21a34bd31e08ae8afd9183125
Robin <robin.enjalbert@inria.fr>
2021-12-17 17:50:41 +0100

    Update getPrediction method for non TcpIp Environments.

--- DeepPhysX_Core/Environment/BaseEnvironment.py
+++ DeepPhysX_Core/Environment/BaseEnvironment.py
@@ -179,10 +179,15 @@ class BaseEnvironment(TcpIpClient):
         """
         if self.as_tcpip_client:
             return self.sync_send_prediction_request(network_input=input_array)
-        if self.environment_manager is not None:
-            return self.environment_manager.requestPrediction(network_input=input_array)
-        raise ValueError(f"[{self.name}] Can't get prediction since Environment has no Manager.")
-
+        if self.environment_manager.data_manager is None:
+            raise ValueError("Cannot request prediction if DataManager does not exist")
+        elif self.environment_manager.data_manager.manager is None:
+            raise ValueError("Cannot request prediction if Manager does not exist")
+        elif self.environment_manager.data_manager.manager.network_manager is None:
+            raise ValueError("Cannot request prediction if NetworkManager does not exist")
+        else:
+            return self.environment_manager.data_manager.manager.network_manager.computeOnlinePrediction(
+                network_input=input_array[None,])
 
     async def get_prediction(self, input_array):
         if self.as_tcpip_client:
