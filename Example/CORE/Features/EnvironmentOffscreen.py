0707358cff3f4648c74ba111fa297be38bb5166f
Robin <robin.enjalbert@inria.fr>
2022-02-17 17:26:28 +0100

    Add Multiprocess example.

--- Example/CORE/Features/EnvironmentOffscreen.py
+++ Example/CORE/Features/EnvironmentOffscreen.py
@@ -6,7 +6,8 @@ The random vector is the input of the network and the ground truth is the mean.
 
 # Python related imports
 from numpy import mean, pi
-from numpy.random import random
+from numpy.random import random, randint
+from time import sleep
 
 # Session related imports
 from Environment import MeanEnvironment
@@ -31,6 +32,13 @@ class MeanEnvironmentOffscreen(MeanEnvironment):
                                  as_tcp_ip_client=as_tcp_ip_client,
                                  environment_manager=environment_manager)
 
+        self.sleep = False
+
+    def recv_parameters(self, param_dict):
+        MeanEnvironment.recv_parameters(self, param_dict)
+        # If True, step will sleep a random time to simulate longer processes
+        self.sleep = param_dict['sleep'] if 'sleep' in param_dict else self.sleep
+
     def send_visualization(self):
         # Return no visualization data
         return {}
@@ -40,6 +48,9 @@ class MeanEnvironmentOffscreen(MeanEnvironment):
         if not self.constant:
             self.input_value = pi * random(self.data_size)
             self.output_value = mean(self.input_value, axis=0)
+        # Simulate longer process
+        if self.sleep:
+            sleep(0.01 * randint(0, 10))
         # Send the training data
         self.set_training_data(input_array=self.input_value,
                                output_array=self.output_value)
--- Example/CORE/Features/README.md
+++ Example/CORE/Features/README.md
@@ -8,6 +8,15 @@ training scripts. Otherwise, you can download training data and / or trained
 network by running ___.
 
 ###Content
+
+Examples of an Environment: how to create it, how to send and receive data, 
+how to initialize and update visualization data, how to send requests.
 * `Environment.py`: Implementation of the Data producer
 * `EnvironmentOffscreen.py`: Same implementation as Environment without visualization
+
+Examples of Dataset generation: how to create the pipeline, how to use multiprocessing 
+to speed up the data production.
+
+
+* 
 * `GradienDescent.py`: A funny example to visualize the gradient descent process
