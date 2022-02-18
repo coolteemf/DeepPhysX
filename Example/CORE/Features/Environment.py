c5f3af44cbb4166fe2debef5a232249a2bfaa5c9
Robin <robin.enjalbert@inria.fr>
2022-02-18 11:21:16 +0100

    Add online training example.

--- Example/CORE/Features/Environment.py
+++ Example/CORE/Features/Environment.py
@@ -74,7 +74,6 @@ class MeanEnvironment(BaseEnvironment):
                                            "c": "orange",
                                            "at": self.instance_id,
                                            "r": 12})
-        print("Visu")
         # Return the visualization data
         return self.factory.objects_dict
 
