a7c61f2a0589fb33f72e073e4d029c9de22bcb71
Robin <robin.enjalbert@inria.fr>
2022-02-21 15:35:39 +0100

    Armadillo Demo in SOFA

---
+++ Example/SOFA/Demos/Armadillo/Environment/ArmadilloPrediction.py
@@ -0,0 +1,77 @@
+"""
+ArmadilloPrediction
+Simulation of an Armadillo with FEM computed simulations.
+The SOFA simulation contains two models of an Armadillo :
+    * one to apply forces and compute deformations
+    * one to apply the network predictions
+Training data are produced at each time step :
+    * input : applied forces on each surface node
+    * output : resulted displacement of each surface node
+"""
+
+# Python imports
+import os
+import sys
+
+# Working session imports
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from ArmadilloTraining import ArmadilloTraining, np
+
+
+class ArmadilloPrediction(ArmadilloTraining):
+
+    def __init__(self,
+                 root_node,
+                 ip_address='localhost',
+                 port=10000,
+                 instance_id=0,
+                 number_of_instances=1,
+                 as_tcp_ip_client=True,
+                 environment_manager=None):
+
+        ArmadilloTraining.__init__(self,
+                                   root_node=root_node,
+                                   ip_address=ip_address,
+                                   port=port,
+                                   instance_id=instance_id,
+                                   number_of_instances=number_of_instances,
+                                   as_tcp_ip_client=as_tcp_ip_client,
+                                   environment_manager=environment_manager)
+
+    def send_visualization(self):
+        """
+        Define and send the initial visualization data dictionary. Automatically called whn creating Environment.
+        """
+
+        # Nothing to visualize since the predictions are run in SOFA GUI.
+        return self.factory.objects_dict
+
+    def onAnimateEndEvent(self, event):
+        """
+        Called within the Sofa pipeline at the end of the time step. Compute training data and apply prediction.
+        """
+
+        # Get a prediction and apply it on NN model
+        input_array = self.compute_input()
+        output_array = self.compute_output()
+
+        # Send training data
+        self.set_training_data(input_array=input_array,
+                               output_array=output_array)
+
+    def apply_prediction(self, prediction):
+        """
+        Apply the predicted displacement to the NN model.
+        """
+
+        # Reshape to correspond regular grid, transform to sparse grid
+        U = np.reshape(prediction, self.output_size) / 10
+        self.n_surface_mo.position.value = self.n_surface_mo.rest_position.array() + U
+
+    def check_sample(self, check_input=True, check_output=True):
+        """
+        Check if the produced sample is correct. Automatically called by DeepPhysX to check sample validity.
+        """
+
+        # See the network prediction even if the solver diverged.
+        return True
---
+++ Example/SOFA/Demos/Armadillo/Environment/parameters.py
@@ -0,0 +1,56 @@
+"""
+Parameters
+Define the sets of parameters :
+    * model parameters
+    * grid parameters
+    * forces parameters
+"""
+
+import os
+import sys
+from numpy import array
+from collections import namedtuple
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from utils import define_bbox, compute_grid_resolution, find_fixed_box, find_extremities, get_nb_nodes
+
+# Model
+mesh = os.path.dirname(os.path.abspath(__file__)) + '/models/armadillo.obj'
+coarse_mesh = os.path.dirname(os.path.abspath(__file__)) + '/models/armadillo_coarse.obj'
+scale = 1e-3
+scale3d = 3 * [scale]
+fixed_box = find_fixed_box(mesh, scale)
+vedo_translation = scale * array([15, 0, 0])
+nb_nodes = get_nb_nodes(coarse_mesh)
+model = {'mesh': mesh,
+         'mesh_coarse': coarse_mesh,
+         'scale': scale,
+         'scale3d': scale3d,
+         'fixed_box': fixed_box,
+         'vedo_translation': vedo_translation.tolist(),
+         'nb_nodes': nb_nodes}
+p_model = namedtuple('p_model', model)(**model)
+
+# Grid
+margin_scale = 0.1
+cell_size = 0.06
+min_bbox, max_bbox, b_box = define_bbox(mesh, margin_scale, scale)
+grid_resolution = compute_grid_resolution(max_bbox, min_bbox, cell_size)
+grid = {'b_box': b_box,
+        'grid_resolution': grid_resolution}
+p_grid = namedtuple('p_grid', grid)(**grid)
+
+# Forces
+zones = ['tail', 'r_hand', 'l_hand', 'r_ear', 'l_ear', 'muzzle']
+centers, radius, amplitude = {}, {}, {}
+for zone, c, rad, amp in zip(zones, find_extremities(mesh, scale), [2.5, 2.5, 2.5, 2., 2., 1.5],
+                             array([15, 2.5, 2.5, 7.5, 7.5, 7.5]) * scale):
+    centers[zone] = c
+    radius[zone] = scale * rad
+    amplitude[zone] = scale * amp
+forces = {'zones': zones,
+          'centers': centers,
+          'radius': radius,
+          'amplitude': amplitude,
+          'simultaneous': 1}
+p_forces = namedtuple('p_forces', forces)(**forces)
