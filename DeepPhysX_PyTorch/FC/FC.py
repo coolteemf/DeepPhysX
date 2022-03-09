18d78cc3db97209330ce9d336c5079f7f2952d82
RobinEnjalbert <robin.enjalbrt@gmail.com>
2022-03-09 14:36:19 +0100

    Add biases in FC architecture. Include FC modules docstrings in docs

--- DeepPhysX_PyTorch/FC/FC.py
+++ DeepPhysX_PyTorch/FC/FC.py
@@ -1,48 +1,56 @@
-from typing import List, Union
-
 import torch
-from torch.nn import Sequential, PReLU, Module, Linear
+from torch.nn import Sequential, PReLU, Linear
+from collections import namedtuple
 
 from DeepPhysX_PyTorch.Network.TorchNetwork import TorchNetwork
 
 
-class FCLayer(Module):
-
-    def __init__(self,
-                 nb_input_channels,
-                 nb_output_channels):
-        """Creates one fully connected layer of dimension nb_input_channels X nb_output_channels"""
-        super().__init__()
-        self.linear = Sequential(Linear(nb_input_channels, nb_output_channels, False))
-
-    def forward(self, input_data):
-        """Gives input_data as raw input to the neural network"""
-        res = self.linear(input_data)
-        return res
-
-
 class FC(TorchNetwork):
+    """
+    | Create a Fully Connected layers Neural Network Architecture.
 
-    def __init__(self, config):
-        """Creates a simple fully connected layers neural network"""
+    :param namedtuple config: Namedtuple containing FC parameters
+    """
+
+    def __init__(self, config: namedtuple):
+
         TorchNetwork.__init__(self, config)
 
-        self.layers: List[Union[FCLayer, PReLU]] = []
-        for i in range(len(self.config.dim_layers) - 1):
-            self.layers.append(FCLayer(nb_input_channels=self.config.dim_layers[i],
-                                       nb_output_channels=self.config.dim_layers[i + 1]))
+        # Convert biases to a List if not already one
+        biases = None
+        if isinstance(config.biases, list):
+            if len(config.biases) != len(self.config.dim_layers) - 1:
+                raise ValueError("Biases list length does not match layers count")
+            biases = config.biases
+        else:
+            biases = [config.biases] * (len(self.config.dim_layers) - 1)
+        # Init the layers
+        self.layers = []
+        for i, bias in enumerate(biases):
+            self.layers.append(Linear(self.config.dim_layers[i], self.config.dim_layers[i + 1], bias))
             self.layers.append(PReLU(num_parameters=self.config.dim_layers[i + 1]))
-        self.linear = Sequential(*(self.layers[:-1]))
+        self.layers = self.layers[:-1]
+        self.linear = Sequential(*self.layers)
 
-    def forward(self, input_data: torch.Tensor) -> torch.Tensor:
-        """Gives input_data as raw input to the neural network"""
-        return self.linear(input_data.view(input_data.shape[0], -1)).view(input_data.shape[0], -1, self.config.dim_output)
+    def forward(self, input_data: torch.Tensor):
+        """
+        | Gives input_data as raw input to the neural network.
+
+        :param torch.Tensor input_data: Input tensor
+        :return: Network prediction
+        """
+
+        res = self.linear(input_data.view(input_data.shape[0], -1)).view(input_data.shape[0], -1,
+                                                                         self.config.dim_output)
+        return res
 
     def __str__(self):
+        """
+        :return: String containing information about the BaseNetwork object
+        """
+
         description = TorchNetwork.__str__(self)
+        description += self.linear.__str__() + "\n"
         description += f"    Layers dimensions: {self.config.dim_layers}\n"
         description += f"    Output dimension: {self.config.dim_output}\n"
-        description += f"    Layers: "
-        for layer in self.layers:
-            description += self.print_architecture(str(layer))
         return description
--- docs/source/api/torch/fc.rst
+++ docs/source/api/torch/fc.rst
@@ -1,2 +1,21 @@
+Fully Connected
+===============
+
 FC
-==
+--
+
+Base:
+:py:class:`TorchNetwork.TorchNetwork`
+
+.. autoclass:: FC.FC
+    :members:
+
+
+TorchNetworkConfig
+---------------------
+
+Base:
+:py:class:`TorchNetworkConfig.TorchNetworkConfig`
+
+.. autoclass:: FCConfig.FCConfig
+    :members:
