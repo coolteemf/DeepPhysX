d8f01e62e056cc859645f5968ec8ce2f35cf13d6
RobinEnjalbert <robin.enjalbrt@gmail.com>
2022-05-06 15:48:54 +0200

    Remove unnecessary conversion

--- DeepPhysX_Core/Manager/NetworkManager.py
+++ DeepPhysX_Core/Manager/NetworkManager.py
@@ -200,7 +200,6 @@ class NetworkManager:
         pred, _ = self.data_transformation.transform_before_loss(pred)
         pred = self.data_transformation.transform_before_apply(pred)
         pred = self.network.transform_to_numpy(pred)
-        pred = array(pred, dtype=float)
         return pred.reshape(-1)
 
     def save_network(self, last_save: bool = False) -> None:
