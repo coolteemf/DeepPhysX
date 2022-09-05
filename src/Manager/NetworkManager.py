ae5b9cf3482c8add889e292981590b4e07319916
coolteemf <francois.lecomte@inria.fr>
2022-09-05 14:20:26 +0200

    621af41d [NetworkManager] remove some conversions in compute_online_prediction

--- src/Manager/NetworkManager.py
+++ src/Manager/NetworkManager.py
@@ -197,7 +197,7 @@ class NetworkManager:
         pred, _ = self.data_transformation.transform_before_loss(pred)
         pred = self.data_transformation.transform_before_apply(pred)
         pred = self.network.transform_to_numpy(pred)
-        return pred.reshape(-1)
+        return pred
 
     def save_network(self, last_save: bool = False) -> None:
         """
