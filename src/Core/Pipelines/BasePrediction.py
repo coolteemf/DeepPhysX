ac66e33b56f9c824e52d9e19724ea475691d6b9e
coolteemf <francois.lecomte@inria.fr>
2023-03-27 14:02:51 +0200

    [BasePrediction] Re-Apply coolteemf changes

--- src/Core/Pipelines/BasePrediction.py
+++ src/Core/Pipelines/BasePrediction.py
@@ -1,168 +1,139 @@
 from typing import Optional
+from os.path import join, exists
 
+from DeepPhysX.Core.Pipelines.BasePipeline import BasePipeline
 from DeepPhysX.Core.Manager.DataManager import DataManager
 from DeepPhysX.Core.Manager.NetworkManager import NetworkManager
-from DeepPhysX.Core.Manager.StatsManager import StatsManager
-from numpy import ndarray
-
 from DeepPhysX.Core.Network.BaseNetworkConfig import BaseNetworkConfig
-from DeepPhysX.Core.Dataset.BaseDatasetConfig import BaseDatasetConfig
+from DeepPhysX.Core.Database.BaseDatabaseConfig import BaseDatabaseConfig
 from DeepPhysX.Core.Environment.BaseEnvironmentConfig import BaseEnvironmentConfig
-from DeepPhysX.Core.Pipelines.BasePipeline import BasePipeline
-from DeepPhysX.Core.Manager.Manager import Manager
+from DeepPhysX.Core.Utils.path import get_first_caller
 
 
-class BaseRunner(BasePipeline):
-    """
-    | BaseRunner is a pipeline defining the running process of an artificial neural network.
-    | It provides a highly tunable learning process that can be used with any machine learning library.
-
-    :param BaseNetworkConfig network_config: Specialisation containing the parameters of the network manager
-    :param BaseEnvironmentConfig environment_config: Specialisation containing the parameters of the environment manager
-    :param Optional[BaseDatasetConfig] dataset_config: Specialisation containing the parameters of the dataset manager
-    :param str session_name: Name of the newly created directory if session_dir is not defined
-    :param Optional[str] session_dir: Name of the directory in which to write all the necessary data
-    :param int nb_steps: Number of simulation step to play
-    :param bool record_inputs: Save or not the input in a numpy file
-    :param bool record_outputs: Save or not the output in a numpy file
-    """
+class BasePrediction(BasePipeline):
 
     def __init__(self,
                  network_config: BaseNetworkConfig,
-                 environment_config: Optional[BaseEnvironmentConfig] = None,
-                 dataset_config: Optional[BaseDatasetConfig] = None,
-                 session_name: str = 'default',
-                 session_dir: Optional[str] = None,
-                 nb_steps: int = 0,
-                 record_inputs: bool = False,
-                 record_outputs: bool = False,
+                 environment_config: BaseEnvironmentConfig,
+                 database_config: Optional[BaseDatabaseConfig] = None,
+                 session_dir: str = 'session',
+                 session_name: str = 'training',
+                 step_nb: int = -1,
+                 record: bool = False,
                  network_manager=NetworkManager,
-                 data_manager=DataManager,
-                 stats_manager=StatsManager
-                 ):
+                 data_manager=DataManager):
+        """
+        BasePrediction is a pipeline defining the running process of an artificial neural network.
+        It provides a highly tunable learning process that can be used with any machine learning library.
+
+        :param network_config: Configuration object with the parameters of the Network.
+        :param environment_config: Configuration object with the parameters of the Environment.
+        :param database_config: Configuration object with the parameters of the Database.
+        :param session_dir: Relative path to the directory which contains sessions repositories.
+        :param session_name: Name of the new the session repository.
+        :param step_nb: Number of simulation step to play.
+        :param record: If True, prediction data will be saved in a dedicated Database.
+        """
 
         BasePipeline.__init__(self,
                               network_config=network_config,
-                              dataset_config=dataset_config,
+                              database_config=database_config,
                               environment_config=environment_config,
-                              session_name=session_name,
                               session_dir=session_dir,
+                              session_name=session_name,
+                              new_session=False,
                               pipeline='prediction')
 
-        self.name = self.__class__.__name__
+        # Define the session repository
+        root = get_first_caller()
+        session_dir = join(root, session_dir)
+        if not exists(join(session_dir, session_name)):
+            raise ValueError(f"[{self.name}] The following directory does not exist: {join(session_dir, session_name)}")
+        self.session = join(session_dir, session_name)
 
-        if type(nb_steps) != int or nb_steps < 0:
-            raise TypeError("[BaseRunner] The number of steps must be a positive int")
+        # Create a NetworkManager
+        self.network_manager = network_manager(network_config=network_config,
+                                              pipeline=self.type,
+                                              session=self.session,
+                                              new_session=False)
 
-        self.nb_samples = nb_steps
-        self.idx_step = 0
+        # Create a DataManager
+        self.data_manager = data_manager(pipeline=self,
+                                        database_config=database_config,
+                                        environment_config=environment_config,
+                                        session=self.session,
+                                        new_session=False,
+                                        produce_data=record,
+                                        batch_size=1)
+        self.data_manager.connect_handler(self.network_manager.get_database_handler())
+        self.network_manager.link_clients(self.data_manager.nb_environment)
 
-        # Tell if data is recording while predicting (output is recorded only if input too)
-        self.record_data = {'input': False, 'output': False}
-        if dataset_config is not None:
-            self.record_data = {'input': record_inputs, 'output': record_outputs and record_inputs}
-        self.is_environment = environment_config is not None
-
-        self.manager = Manager(pipeline=self,
-                               network_config=self.network_config,
-                               dataset_config=dataset_config,
-                               environment_config=self.environment_config,
-                               session_name=session_name,
-                               session_dir=session_dir,
-                               new_session=True,
-                               network_manager=network_manager,
-                               data_manager=data_manager,
-                               stats_manager=stats_manager)
+        # Prediction variables
+        self.step_nb = step_nb
+        self.step_id = 0
 
     def execute(self) -> None:
         """
-        | Main function of the running process "execute" call the functions associated with the learning process.
-        | Each of the called functions are already implemented so one can start a basic run session.
-        | Each of the called function can also be rewritten via inheritance to provide more specific / complex running
-          process.
+        Launch the prediction Pipeline.
+        Each event is already implemented for a basic pipeline but can also be rewritten via inheritance to describe a
+        more complex Pipeline.
         """
 
-        self.run_begin()
-        while self.running_condition():
+        self.prediction_begin()
+        while self.prediction_condition():
             self.sample_begin()
-            self.sample_end(self.predict())
-        self.run_end()
+            self.predict()
+            self.sample_end()
+        self.prediction_end()
 
-    def predict(self, animate: bool = True) -> ndarray:
+    def prediction_begin(self) -> None:
         """
-        | Pull the data from the manager and return the prediction
-
-        :param bool animate: True if getData fetch from the environment
-        :return: Prediction from the Network
-        """
-
-        self.manager.get_data(animate=animate)
-        data = self.manager.data_manager.data['input']
-        data = self.manager.data_manager.normalize_data(data, 'input')
-        return self.manager.network_manager.compute_online_prediction(data)
-
-    def run_begin(self) -> None:
-        """
-        | Called once at the very beginning of the Run process.
-        | Allows the user to run some pre-computations.
+        Called once at the beginning of the prediction Pipeline.
         """
 
         pass
 
-    def run_end(self) -> None:
+    def prediction_condition(self) -> bool:
         """
-        | Called once at the very end of the Run process.
-        | Allows the user to run some post-computations.
-        """
-
-        pass
-
-    def running_condition(self) -> bool:
-        """
-        | Condition that characterize the end of the running process.
-
-        :return: False if the training needs to stop.
+        Condition that characterize the end of the prediction Pipeline.
         """
 
-        running = self.idx_step < self.nb_samples if self.nb_samples > 0 else True
-        self.idx_step += 1
+        running = self.step_id < self.step_nb if self.step_nb > 0 else True
+        self.step_id += 1
         return running
 
     def sample_begin(self) -> None:
         """
-        | Called one at the start of each step.
-        | Allows the user to run some pre-step computations.
+        Called one at the beginning of each sample.
         """
 
         pass
 
-    def sample_end(self, prediction: ndarray) -> None:
+    def predict(self) -> None:
         """
-        | Called one at the end of each step.
-        | Allows the user to run some post-step computations.
-
-        :param ndarray prediction: Prediction of the Network.
+        Pull the data from the manager and return the prediction.
         """
 
-        if self.is_environment:
-            self.manager.data_manager.apply_prediction(prediction)
+        self.data_manager.get_data(epoch=0,
+                                   animate=True)
 
-    def close(self) -> None:
+    def sample_end(self) -> None:
         """
-        | End the running process and close all the managers
-        """
-
-        self.manager.close()
-
-    def __str__(self) -> str:
-        """
-        :return: str Contains running information about the running process
+        Called one at the end of each sample.
         """
 
-        description = ""
-        description += f"Running statistics :\n"
-        description += f"Number of simulation step: {self.nb_samples}\n"
-        description += f"Record inputs : {self.record_data[0]}\n"
-        description += f"Record outputs : {self.record_data[1]}\n"
+        pass
 
+    def prediction_end(self) -> None:
+        """
+        Called once at the end of the prediction Pipeline.
+        """
+
+        self.data_manager.close()
+        self.network_manager.close()
+
+    def __str__(self):
+
+        description = BasePipeline.__str__(self)
+        description += f"   Number of step: {self.step_nb}\n"
         return description
